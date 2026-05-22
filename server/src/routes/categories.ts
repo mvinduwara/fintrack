09f204aadfec0455077519ed35a81854cf624f6c
mvinduwara <mvinduwara@gmail.com>
2026-05-22 09:30:38 +0530

    Create categories.ts

---
+++ server/src/routes/categories.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import { getCategories, createCategory, updateCategory, deleteCategory } from '../controllers/categoryController';
+import { authenticate } from '../middleware/auth';
+const router = Router();
+router.use(authenticate);
+router.get('/', getCategories);
+router.post('/', createCategory);
+router.put('/:id', updateCategory);
+router.delete('/:id', deleteCategory);
+export default router;
